cdbce489d76596258babd1865ef7f3e29785a209
MostofaAsik <mmostofaasik@gmail.com>
2023-05-02 12:15:56 +0600

    tooggle login logout

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 
@@ -8,20 +8,39 @@ export const AuthContext = createContext(null)
 const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
-    const user = { diaplayNamame: 'MAS' }
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     const createUser = (email, password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const logIn = (email, password) => {
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
+
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        });
+        return () => {
+            return unsubscribe;
+        }
+    }, [])
 
     const authInfo = {
         user,
         createUser,
         logIn,
+        logOut
 
     }
     return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,13 @@ import { NavLink } from 'react-router-dom';
 import { AuthContext } from '../../Provider/AuthProvider';
 
 const Navbar = () => {
-    const { user } = useContext(AuthContext)
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogout = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.log(error.message))
+    }
     return (
         <div className=''>
             <div className="navbar bg-neutral text-neutral-content flex justify-between ">
@@ -14,10 +20,10 @@ const Navbar = () => {
 
                     <NavLink className='mr-3' to='/'>Home</NavLink>
                     <NavLink className='mr-3' to='/blog'>Blog</NavLink>
-                    <button className='mr-3'>LogOut</button>
-                    <NavLink className='mr-3' to='/login'>LogIn</NavLink>
+                    <button onClick={handleLogout} className='mr-3'>LogOut</button>
+                    {/* <NavLink className='mr-3' to='/login'>LogIn</NavLink> */}
 
-                    {/* {user ? <img src='https://media.istockphoto.com/id/1170902285/photo/portrait-of-a-cool-guy-pointing-and-looking-up-impressed.jpg?s=612x612&w=0&k=20&c=Fs3iFBD-b6-21v3CgSdtFsqwiEVU-34WpTKxORJ7qVc=' alt="" className=' rounded-full w-10 h-10'></img> : <NavLink className='mr-3' to='/login'>LogIn</NavLink>} */}
+                    {user ? <img src='https://media.istockphoto.com/id/1170902285/photo/portrait-of-a-cool-guy-pointing-and-looking-up-impressed.jpg?s=612x612&w=0&k=20&c=Fs3iFBD-b6-21v3CgSdtFsqwiEVU-34WpTKxORJ7qVc=' alt="" className=' rounded-full w-10 h-10'></img> : <NavLink className='mr-3' to='/login'>LogIn</NavLink>}
 
 
                 </div>
