0dfd0f44da3f26eea3a68c9bbc5d001626faee6b
MostofaAsik <mmostofaasik@gmail.com>
2023-05-03 23:37:18 +0600

    homepage done

--- src/components/HomeSection/HomeSection2.jsx
+++ src/components/HomeSection/HomeSection2.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 const HomeSection2 = () => {
     return (
-        <div className="w-64 h-64 bg-gray-300 relative text-center">
-            <img
-                src="https://media.istockphoto.com/id/1034391536/photo/portrait-of-cheerful-excited-joyful-satisfied-handsome-attractive-fashionable-macho-guy.jpg?s=1024x1024&w=is&k=20&c=eXh2DiIOZcgBVyDCgkCIyEIMLj49g1hupRExZTV_jns="
-                alt="placeholder image"
-                className="absolute inset-0 w-full h-full object-cover transition-transform duration-500 transform hover:scale-110"
-            />
-            <div className="absolute inset-0 flex items-center justify-center text-white text-2xl font-bold uppercase tracking-wider transition-transform duration-500 transform hover:translate-x-6 hover:translate-y-6">
-                Hover Text
+        <div className='bg-lime-200 p-10'>
+            <h1 className='text-center text-4xl font-bold mt-4'>Md Jahid Iqbal</h1>
+            <div className="w-64 h-64 bg-gray-300 relative text-center mx-auto">
+                <img
+                    src="https://media.istockphoto.com/id/1034391536/photo/portrait-of-cheerful-excited-joyful-satisfied-handsome-attractive-fashionable-macho-guy.jpg?s=1024x1024&w=is&k=20&c=eXh2DiIOZcgBVyDCgkCIyEIMLj49g1hupRExZTV_jns="
+                    alt="placeholder image"
+                    className="absolute inset-0 w-full h-full object-cover transition-transform duration-500 transform hover:scale-110"
+                />
+                <div className="absolute inset-0 flex items-center justify-center text-white text-2xl font-bold uppercase tracking-wider transition-transform duration-500 transform hover:translate-x-6 hover:translate-y-6">
+                    OWNER:
+                </div>
             </div>
         </div>
     );
